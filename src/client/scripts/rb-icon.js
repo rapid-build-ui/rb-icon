15ed3c66e9a2324637094a3d19f57cced79f1b10
Rapid Build UI <rapidbuildui@yahoo.com>
2018-06-13 01:36:01 -0500

    fix(safari): remove the use tag in lieu of dynamically updating the svg viewBox attr and it's path d attr relevant to kind and or source

--- src/client/scripts/rb-icon.js
+++ src/client/scripts/rb-icon.js
@@ -11,20 +11,37 @@ export class RbIcon extends PolymerElement {
 	constructor() {
 		super();
 	}
+	connectedCallback() {
+		super.connectedCallback();
+		this._elms = {
+			icon: {
+				elm:     null,
+				viewBox: null
+			},
+			path: {
+				elm: null,
+				d:   null
+			}
+		};
+		this._elms.icon.elm = this.root.querySelector('#rb-icon');
+		this._elms.path.elm = this._elms.icon.elm.querySelector('path');
+	}
 
 	/* Properties
 	 ************/
 	static get properties() {
 		return {
 			kind:	{
-				type: String
+				type: String,
+				observer: '_iconChanged'
 			},
 			size: {
 				type: Number
 			},
 			source:	{
 				type: String,
-				value: 'default'
+				value: 'default',
+				observer: '_iconChanged'
 			}
 		}
 	}
@@ -37,6 +54,64 @@ export class RbIcon extends PolymerElement {
 	_source(source, key) { // :boolean
 		if (!source && key === 'default') return true;
 		return source === key;
+	}
+
+	/* Observers
+	 ************/
+	_iconChanged(newVal, oldVal) { // :void
+		// timeout to ensure template is rendered
+		setTimeout(() => {
+			if (!this._elms) return;
+			const newElms = {
+				icon: {
+					elm:     null,
+					viewBox: null
+				},
+				path: {
+					elm: null,
+					d:   null
+				}
+			};
+
+			// set id selector
+			let id = this.source || 'default';
+				id = `#${id}-icons #${this.kind}`;
+
+			if (id.includes('{')) return; // invalid css selector TODO: polish
+
+			// set icon elm
+			newElms.icon.elm = this.root.querySelector(id);
+
+			// handle invalid icon kind and or source
+			if (!newElms.icon.elm) {
+				this._elms.icon.elm.classList.add('hide');
+				this._elms.icon.elm.removeAttribute('viewBox');
+				this._elms.path.elm.removeAttribute('d');
+				return;
+			} else {
+				this._elms.icon.elm.classList.remove('hide');
+			}
+
+			// set path elm
+			newElms.path.elm = newElms.icon.elm.querySelector('path');
+
+			// set attrs
+			newElms.icon.viewBox = newElms.icon.elm.getAttribute('viewBox');
+			newElms.path.d       = newElms.path.elm.getAttribute('d');
+
+			const isActiveIcon = newElms.icon.viewBox === this._elms.icon.viewBox &&
+								 newElms.path.d       === this._elms.path.d;
+
+			if (isActiveIcon) return; // icon didn't change
+
+			// update global elms in dom
+			this._elms.icon.elm.setAttribute('viewBox', newElms.icon.viewBox);
+			this._elms.path.elm.setAttribute('d',       newElms.path.d);
+
+			// update global elms props
+			this._elms.icon.viewBox = newElms.icon.viewBox;
+			this._elms.path.d       = newElms.path.d;
+		});
 	}
 
 	/* Template
