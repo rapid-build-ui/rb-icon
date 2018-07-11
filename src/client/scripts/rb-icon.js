1830f47544de48f19328020f1a258edcff988064
Rapid Build UI <rapidbuildui@yahoo.com>
2018-07-10 23:25:42 -0500

    fix(icon occasionally blinking): on initial load
    Use viewReady() instead of childrenUpdated() to load in the icon.

--- src/client/scripts/rb-icon.js
+++ src/client/scripts/rb-icon.js
@@ -20,26 +20,24 @@ export class RbIcon extends withComponent(withRenderer()) {
 
 	/* Lifecycle
 	 ************/
-	childrenUpdated() { // :void (timeout to ensure template is rendered)
-		setTimeout(() => {
-			let spriteSvg    = null;
-			let spritePath   = null;
-			const activeSvg  = this.shadowRoot.querySelector('.rb-icon');
-			const activePath = activeSvg.querySelector('path');
-			try { // incase of invalid css selector
-				spriteSvg = this.shadowRoot.querySelector(`#${this.props.kind}`);
-			} catch {}
-			if (!spriteSvg) {
-				activeSvg.classList.add('hide');
-				activeSvg.removeAttribute('viewBox');
-				activePath.removeAttribute('d');
-				return;
-			}
-			activeSvg.classList.remove('hide')
-			spritePath = spriteSvg.querySelector('path');
-			activeSvg.setAttribute('viewBox', spriteSvg.getAttribute('viewBox'));
-			activePath.setAttribute('d', spritePath.getAttribute('d'));
-		});
+	viewReady() {
+		let spriteSvg    = null;
+		let spritePath   = null;
+		const activeSvg  = this.shadowRoot.querySelector('.rb-icon');
+		const activePath = activeSvg.querySelector('path');
+		try { // incase of invalid css selector
+			spriteSvg = this.shadowRoot.querySelector(`#${this.props.kind}`);
+		} catch {}
+		if (!spriteSvg) {
+			activeSvg.classList.add('hide');
+			activeSvg.removeAttribute('viewBox');
+			activePath.removeAttribute('d');
+			return;
+		}
+		activeSvg.classList.remove('hide')
+		spritePath = spriteSvg.querySelector('path');
+		activeSvg.setAttribute('viewBox', spriteSvg.getAttribute('viewBox'));
+		activePath.setAttribute('d', spritePath.getAttribute('d'));
 	}
 
 	/* Template
